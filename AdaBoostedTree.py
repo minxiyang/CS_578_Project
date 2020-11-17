0cdfca2aabbb70652bb526e610415e718dce8959
[Minxi <[yhzz598@gmail.com]>
2020-11-17 18:26:03 -0500

    update

--- AdaBoostedTree.py
+++ AdaBoostedTree.py
@@ -8,7 +8,7 @@ import numpy as np
 import math
 
 
-def plot_ROC(model,x,y,w):
+def ROC(model,x,y,w):
 	
 	FPR=np.ones(0)
 	TPR=np.ones(0)
@@ -37,75 +37,83 @@ def plot_ROC(model,x,y,w):
 	return [AUC, FPR, TPR]
 	
 			
-#load data				
-
-train_data = pd.read_csv("./data/trainingSet.csv")
-train_data.loc[train_data['Label']=='s',['Weight']]= train_data.loc[train_data['Label']=='s',['Weight']]/123.02614
-train_data = train_data.sample(frac=1)
-test_data = pd.read_csv("./data/testingSet.csv")
-x_train = train_data.drop(['EventId','Weight','Label'], axis=1)
-w_train = train_data['Weight']
-y_train = train_data['Label']
-x_test = test_data.drop(['EventId','Weight','Label'], axis=1)
-w_test = test_data['Weight']
-y_test = test_data['Label']
-
-
-#define model 
-
-bdt=AdaBoostClassifier(DecisionTreeClassifier(max_depth=5),n_estimators=50,learning_rate=1)
 
 # k-fold cross vaildation
+def kfold(k,bdt,x,y,w):
 
-k=5
-n=len(x_train)
-d=round(n/k)
-k_fold_idx=[]
+	n=len(x)
+	d=round(n/k)
+	k_fold_idx=[]
 
-print ('start k-fold cross vaildation')
-for i in range(k):
+	print ('start k-fold cross vaildation')
+	for i in range(k):
 	
-	test_idx=range(d*i,d*(i+1))
-	train_idx=np.setdiff1d(range(0,n),test_idx)
-	k_fold_idx.append([train_idx,test_idx])
+		test_idx=range(d*i,d*(i+1))
+		train_idx=np.setdiff1d(range(0,n),test_idx)
+		k_fold_idx.append([train_idx,test_idx])
 
-AUCs=[]
-FPRs=[]
-TPRs=[]
+	AUCs=[]
+	FPRs=[]
+	TPRs=[]
 
-for i in range(k):
+	for i in range(k):
 	
-	print ('run %s time k-fold' %str(i+1))
-	[train_idx,test_idx]=k_fold_idx[i]
-	x_train_fold=x_train.iloc[train_idx]
-	y_train_fold=y_train.iloc[train_idx]
-	w_train_fold=w_train[train_idx]
-	x_test_fold=x_train.iloc[test_idx]
-	y_test_fold=y_train.iloc[test_idx]
-	w_test_fold=w_train.iloc[test_idx]
-	model=clone(bdt)
-	model.fit(x_train_fold,y_train_fold,sample_weight=w_train_fold)
-	[AUC,FPR,TPR]=plot_ROC(model,x_test_fold,y_test_fold,w_test_fold)
-	AUCs.append(AUC)
-	FPRs.append(FPR)
-	TPRs.append(TPR)
+		print ('run %s time k-fold' %str(i+1))
+		[train_idx,test_idx]=k_fold_idx[i]
+		x_fold=x.iloc[train_idx]
+		y_fold=y.iloc[train_idx]
+		w_fold=w[train_idx]
+		x_test_fold=x.iloc[test_idx]
+		y_test_fold=y.iloc[test_idx]
+		w_test_fold=w.iloc[test_idx]
+		model=clone(bdt)
+		model.fit(x_fold,y_fold,sample_weight=w_fold)
+		[AUC,FPR,TPR]=ROC(model,x_test_fold,y_test_fold,w_test_fold)
+		AUCs.append(AUC)
+		FPRs.append(FPR)
+		TPRs.append(TPR)
 
-print ("k-fold result")
+	print ("k-fold result")
 
-AUC_var=np.var(AUCs)
-AUC_mean=sum(AUCs)/len(AUCs)
+	AUC_var=np.var(AUCs)
+	AUC_mean=sum(AUCs)/len(AUCs)
 
-print('variance for AUC is '+str(AUC_var))
-print('mean for AUC is '+str(AUC_mean))
+	print('variance for AUC is '+str(AUC_var))
+	print('mean for AUC is '+str(AUC_mean))
+	return [AUC_mean,AUC_var,FPRs,TPRs]
 
-#plot ROC
+if __name__=="__main__":
+	
+	#load data                              
 
-plt.xlabel("FP rate")
-plt.ylabel("TP rate")
+	train_data = pd.read_csv("./data/trainingSet.csv")
+	train_data.loc[train_data['Label']=='s',['Weight']]= train_data.loc[train_data['Label']=='s',['Weight']]/123.02614
+	train_data = train_data.sample(frac=1)
+	test_data = pd.read_csv("./data/testingSet.csv")
+	x_train = train_data.drop(['EventId','Weight','Label'], axis=1)
+	w_train = train_data['Weight']
+	y_train = train_data['Label']
+	x_test = test_data.drop(['EventId','Weight','Label'], axis=1)
+	w_test = test_data['Weight']
+	y_test = test_data['Label']
 
-for i in range(k):
+	#define model 
 
-	plt.plot(FPRs[i],TPRs[i])
+	bdt=AdaBoostClassifier(DecisionTreeClassifier(max_depth=5),n_estimators=50,learning_rate=1)
+	
+	# k-fold cross vaildation
 
-plt.savefig("plots/AdaBoostTree/ROC_kfold.pdf")
+	k=5
+	
+	[AUC_mean,AUC_var,FPRs,TPRs]=kfold(k,bdt,x_train,y_train,w_train)
+	#plot ROC
+
+	plt.xlabel("FP rate")
+	plt.ylabel("TP rate")
+	
+	for i in range(k):
+
+		plt.plot(FPRs[i],TPRs[i])
+
+	plt.savefig("plots/AdaBoostTree/ROC_kfold.pdf")
 
